30c6e2c83c7e70e1ed5374f26cfc6ceaf7899ba3
Andrew Zolintakis <peterzolintakis@Peters-MacBook-Pro-3.local>
2017-03-30 18:21:12 -0700

    Word Cloud creates article list from both acm and ieee

--- src/backEnd/WordCloud.php
+++ src/backEnd/WordCloud.php
@@ -21,7 +21,22 @@ class WordCloud
     }
 
     public function initializeArticleList($searchWord, $type, $articleCount){
+        $this->articleList = array();
         $IEEEArticleList = $this->getArticleListIEEE($searchWord, $type, $articleCount);
+        $acmArticleList = $this->getArticleListACM($searchWord, $type, $articleCount);
+        $i = 0;
+        while($i < $articleCount/2 && $i < sizeof($IEEEArticleList)) {
+            array_push($this->articleList, $IEEEArticleList[$i]);
+            $i ++;
+        }
+
+        $i = 0;
+        while($i < $articleCount/2 && $i < sizeof($acmArticleList)) {
+            array_push($this->articleList, $acmArticleList[$i]);
+            $i++;
+        }
+
+        print_r($this->articleList);
     }
 
     private function getArticleListIEEE($searchWord, $type, $articleCount)
@@ -30,28 +45,83 @@ class WordCloud
         $url = "http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?";
         if ($type == "author") {
             $author = str_replace(" ", "+", $searchWord);
-            $url = $url . "au=" . $author . "&hc=" . $articleCount;
+            $url = $url . "au=$author";
+        }else{
+            $keyword = str_replace(" ", "+", $searchWord);
+            $url = $url . "querytext=$keyword";
         }
-        
+        $url = $url. "&hc=$articleCount";
+
         $xml = simplexml_load_file($url);
 
+       if(empty($xml))
+           return $IEEEArticleList;
 
-        for($i = 0; $i <$articleCount; $i++){
+        $i = 0;
+        while($i<$articleCount && $i < sizeof($xml->document)){
             $title = (string) $xml->document[$i]->title;
-            $authors = (string) $xml->document[$i]->authors;
+            $authors = explode("; ",(string) $xml->document[$i]->authors);
             $conferences = (string) $xml->document[$i]->pubtitle;
             $pdfUrl = (string) $xml->document[$i]->pdf;
             $source = Constants::IEEE;
             $article = new Article($pdfUrl, $authors, $conferences, $title, $source);
-            //echo "Title: ".$article->name. "\nauthors: ".$article->authors."\nconferences: ".$article->conferences."\nurl: ".$article->url. "\ndatabase: ".$article->database;
             array_push($IEEEArticleList, $article);
+            $i++;
+        }
+        return $IEEEArticleList;
+    }
+
+
+    private function getArticleListACM($searchWord, $type, $articleCount)
+    {
+        $acmArticleList = array();
+        $url = "http://api.crossref.org/works?filter=member:320&";
+        if ($type == "author") {
+            $author = str_replace(" ", "+", $searchWord);
+            $url = $url ."query.author=$author";
+        }else{
+            $keyword = str_replace(" ", "+", $searchWord);
+            $url = $url . "query=$keyword";
         }
 
-        return $IEEEArticleList;
+        $url = $url. "&sort=score&order=desc&rows=$articleCount";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        $json = curl_exec($ch);
+        curl_close($ch);
+
+        $acmArray = json_decode($json, true);
+
+
+
+        $i = 0;
+        while($i < $articleCount && $i < sizeof($acmArray["message"]["items"])){
+            $title = $acmArray["message"]["items"][$i]["title"][0];
+            $authors = array();
+            foreach($acmArray["message"]["items"][$i]["author"] as $author){
+                $name = $author["given"];
+                $name = "$name ".$author["family"];
+                array_push($authors, $name);
+            }
+            $conferences = $acmArray["message"]["items"][$i]["container-title"][0];
+            $articleUrl = $acmArray["message"]["items"][$i]["URL"];
+            $source = Constants::ACM;
+            $article = new Article($articleUrl, $authors, $conferences, $title, $source);
+            array_push($acmArticleList, $article);
+            $i++;
+        }
+        return $acmArticleList;
     }
 
 }
 
 
+
+
 $wc =new WordCloud();
-$wc->initializeArticleList("Andrea Zanella", "author", 20);
+$wc->initializeArticleList("andrea zanella", "author", 10);
