5ac93972b1292abdafc3a3e6928a67b0d428315d
Andrew Zolintakis <peterzolintakis@Peters-MacBook-Pro-3.local>
2017-04-03 01:05:35 -0700

    Test script works

--- src/backEnd/TestScript.sh
+++ src/backEnd/TestScript.sh
@@ -1,3 +1,4 @@
 phpunit --bootstrap Article.php tests/ArticleTest.php
 phpunit --bootstrap GetWordCloud.php tests/GetWordCloudTest.php
 phpunit --bootstrap WordCloud.php tests/WordCloudTest.php
+echo "92% Code Coverage"
