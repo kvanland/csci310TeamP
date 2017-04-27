3bf96df1b3bafdf9e5431a577e9b23ccc5706be0
Alec Schule <aschule@usc.edu>
2017-04-26 19:21:50 -0700

    finished implementing PDF parsing for word cloud generation

---
+++ src/backEnd/PDFParser.php
@@ -0,0 +1,26 @@
+<?php
+
+include '../vendor/autoload.php';
+
+class PDFParser 
+{
+    private $parser;
+
+    function __construct() {
+        $this->parser = new \Smalot\PdfParser\Parser();
+    }
+
+    public function parsePDF($path) {
+        try {
+            $pdf_contents = file_get_contents($path);
+            $pdf = $this->parser->parseContent($pdf_contents);
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            return null;
+        }
+        $text = $pdf->getText();
+        return $text;
+    }
+}
+
+    
